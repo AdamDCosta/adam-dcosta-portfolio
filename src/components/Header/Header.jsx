73d3cca64db27c11183394894ca84075d27b532a
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-31 14:45:00 +0000

    header file created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        Hello, I'm 
+        <span className="header__title--name">Adam</span>
+      </h1>
+      <p></p>
+
+    </header>
+  );
+};
+
+export default Header;

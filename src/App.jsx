d972f83be5d5d6d415d815866ad79448ac34b258
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-31 10:56:15 +0000

    color variables added default react icons deleted

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
